5c966fee6c87c4a8bbaaeb640f8b68e95122d9bc
Frederik Bode <thorbensen@gmail.com>
2023-09-08 15:17:33 +0200

    feat(tw): flex

--- src/components/ChatArea.tsx
+++ src/components/ChatArea.tsx
@@ -7,22 +7,15 @@ export const ChatArea = () => {
         Order notes
       </label>
 
-      <div className="overflow-hidden rounded-lg border border-gray-200 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600">
+      <div className="overflow-hidden flex flex-row align-center rounded-lg border border-gray-200 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600">
         <textarea
           id="OrderNotes"
-          className="w-full resize-none border-none align-top focus:ring-0 sm:text-sm"
-          rows={4}
+          className="w-full resize-none border-none align-center focus:ring-0 sm:text-sm"
+          rows={1}
           placeholder="Chat away!"
         ></textarea>
 
         <div className="flex items-center justify-end gap-2 bg-white p-3">
-          <button
-            type="button"
-            className="rounded bg-gray-200 px-3 py-1.5 text-sm font-medium text-gray-700 hover:text-gray-600"
-          >
-            Clear
-          </button>
-
           <button
             type="button"
             className="rounded bg-indigo-600 px-3 py-1.5 text-sm font-medium text-white hover:bg-indigo-700"
